b95d42f73727aa44327394c9dd3684bd2a487bbe
ShahadilMunawir <shahadilmunawir110@gmail.com>
2022-06-22 00:22:15 +0530

    fixed main.py not found error

--- github_automation.sh
+++ github_automation.sh
@@ -1,11 +1,12 @@
 function gitu(){
-    cd
-    cd Desktop/Python/
+    cd ~/Desktop/Python/GitHub\ Automation/
+    python3 main.py $1
+    cd ..
     mkdir $1
     cd $1
-    python3 main.py $1
     touch README.md
     git init
+    git remote add origin git@github.com:ShahadilMunawir/$1.git
     git add .
     git commit -m "initial commit"
     git push -u origin master

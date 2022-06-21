c027af58382bdd879b82c1022b7b0d0463c3a20b
ShahadilMunawir <shahadilmunawir110@gmail.com>
2022-06-22 00:27:17 +0530

    added virtual enviorment option

--- github_automation.sh
+++ github_automation.sh
@@ -4,6 +4,8 @@ function gitu(){
     cd ..
     mkdir $1
     cd $1
+    virtualenv env
+    source ./env/bin/activate
     touch README.md
     git init
     git remote add origin git@github.com:ShahadilMunawir/$1.git
